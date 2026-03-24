0d99ec780c8afddc65b99340a2366f82fcd400a1
Katsumi <lesanonyme1001@gmail.com>
2026-03-24 18:47:08 +0700

    Edit `models.py`
    Expand model definition

--- src/models.py
+++ src/models.py
@@ -1,31 +1,18 @@
 from datetime import datetime
-
-import sqlalchemy
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    task = db.relationship("Task", backref="user", lazy=True)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    schedules = db.relationship("Schedule", backref="task", lazy=True)
 
 class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     scheduled_time = db.Column(db.DateTime, default=datetime.utcnow)
-    task_id = db.Column(db.Integer, db.ForeignKey("task.id"))
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("task.id"), nullable=False)
